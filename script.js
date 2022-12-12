76f7d7669899328d527ea4d939a08d385e50e995
Jevi Ariyanti <jeviariyanti1801@gmail.com>
2022-12-12 16:12:57 +0700

    k6 performance

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-import { check, group } from "k6";
-import http from "k6/http"
+import { check, group } from 'k6';
+import http from 'k6/http'
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
+export let options = {
+    stages: [
+        {duration: "2s", target: 10}, //Load
+        {duration: "2s", target: 15}, //Stress
+        {duration: "2s", target: 15}, // Stress
+        {duration: "5s", target: 10}, //Load
+        {duration: "1s", target: 50}, //Spike
+        {duration: "5s", target: 10}, //Load
+    ],
+};
 
 export default function() {
     group('K6 Get Test', () => {
@@ -8,4 +21,11 @@ export default function() {
             'is status 200': (r) => r.status ==200
         })
     })
+}
+
+export function handleSummary(data) {
+  return {
+    "result.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  };
 }
